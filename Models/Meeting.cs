0b147816fb02f3cb3aef7c8731c03f4abb89c13e
Craig Freeze <craig.freeze@icloud.com>
2024-04-05 00:26:14 -0700

    Validation set up

--- Models/Meeting.cs
+++ Models/Meeting.cs
@@ -14,49 +14,74 @@ namespace Sacrament_Meeting_Planner.Models
         }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Meeting Day")]
         public DateTime Date
         {
             get; set;
         }
 
+        [Required]
+        [RegularExpression(@"[A-Za-z ]+", ErrorMessage = "Letters and spaces only")]
         public string? Presiding
         {
             get; set;
         }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z ]+", ErrorMessage = "Letters and spaces only")]
         public string? Conducting
         {
             get; set;
         }
 
+        [Required]
+        [Display(Name = "Opening Hymn")]
+        [RegularExpression(@"#[0-9]{1,3}\s[A-Za-z ]+", ErrorMessage = "Number Sign, then 1-3 digits, then space, then hymn title")]
         public string? OpeningHymn
         {
             get; set;
         }
 
+        [Required]
+        [RegularExpression(@"[A-Za-z ]+", ErrorMessage = "Letters and spaces only")]
         public string? Invocation
         {
             get; set;
         }
 
+        [Required]
+        [Display(Name = "Sacrament Hymn")]
+        [RegularExpression(@"#[0-9]{1,3}\s[A-Za-z ]+", ErrorMessage = "Number Sign, then 1-3 digits, then space, then hymn title")]
         public string? SacramentHymn
         {
             get; set;
         }
 
+        [Display(Name = "Intermediate Hymn")]
+        [RegularExpression(@"#[0-9]{1,3}\s[A-Za-z ]+", ErrorMessage = "Number Sign, then 1-3 digits, then space, then hymn title")]
         public string? IntermediateHymn
         {
             get; set;
         }
 
+        [Required]
+        [Display(Name = "Closing Hymn")]
+        [RegularExpression(@"#[0-9]{1,3}\s[A-Za-z ]+", ErrorMessage = "Number Sign, then 1-3 digits, then space, then hymn title")]
         public string? ClosingHymn
         {
             get; set;
         }
 
+        [Required]
+        [RegularExpression(@"[A-Za-z ]+", ErrorMessage = "Letters and spaces only")]
         public string? Benediction
         {
             get; set;
         }
+
+        [Required]
+        [Display(Name = "Speakers' topic")]
+        [RegularExpression(@"[A-Za-z ]+", ErrorMessage = "Letters and spaces only")]
         public string? SpeakerSubject
         {
             get; set;
@@ -75,6 +100,7 @@ namespace Sacrament_Meeting_Planner.Models
         {
             get; set;
         }
+        [RegularExpression(@"[A-Za-z ]+", ErrorMessage = "Letters and spaces only")]
         public string Name
         {
             get; set;
